ad7b78c95737db510e98917e6c290cea6d3a23ce
gokula <gokulakrishnanr812@gmail.com>
2025-04-20 23:40:38 +0530

    Remove deprecated files including controllers, models, and routes related to chat, notifications, ads, comments, videos, and user interactions. This cleanup enhances project maintainability and reduces unnecessary code.

--- dist/service/BaseController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseController {
-    constructor(socket) {
-        this.socket = socket;
-    }
-}
-exports.default = BaseController;
